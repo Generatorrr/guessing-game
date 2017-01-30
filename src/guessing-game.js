34e433a38b2129ed2d36778f66ee46d86c14b491
Generatorr <martinkov1994@mail.ru>
2017-01-31 00:45:09 +0300

    guessing-game/exerscise complete

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+    	this.down = min;
+    	this.up = max;
     }
 
     guess() {
+    	return Math.round((this.down + this.up) / 2);
 
     }
 
     lower() {
-
+    	return this.setRange(this.down, this.guess());
     }
 
     greater() {
-
+    	return this.setRange(this.guess(), this.up);
     }
 }
 
